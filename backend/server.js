9624be4b7f3911c465b307be2cd295c3d1a4a4dd
goncalvesanacarol76-lab <goncalves.anacarol76@gmail.com>
2025-11-13 20:15:54 -0300

    Update server.js

--- backend/server.js
+++ backend/server.js
@@ -10,7 +10,7 @@ import multer from 'multer';
 import 'dotenv/config';
 
 const app = express();
-const port = 5000;
+const port = process.env.PORT || 5000;
 const pump = promisify(pipeline);
 
 // Configuração CORS CRÍTICA: Permite todas as origens
